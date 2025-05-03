1636d537704afdd66f0dcf5b51386a0d1347165a
srikkd <saurabhgupta25dec@gmail.com>
2025-05-03 18:14:53 +0530

    minor change to avoid error

--- src/com/example/designPatterns/c2_builder/v0_telescopic_constructors/Client.java
+++ src/com/example/designPatterns/c2_builder/v0_telescopic_constructors/Client.java
@@ -3,7 +3,7 @@ package com.example.designPatterns.c2_builder.v0_telescopic_constructors;
 public class Client {
     public static void main(String[] args) {
 
-        Student s3 = new Student("Riya", "9728973982", "riya@gmail.com");
+//        Student s3 = new Student("Riya", "9728973982", "riya@gmail.com");
 
     }
 }
